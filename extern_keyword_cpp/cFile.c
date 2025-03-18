b68c301a722fbfcf2ccde5d0f902625307702c0e
ramesh <rameshbe052001@gmail.com>
2025-03-18 20:27:42 +0530

    extern usage

---
+++ extern_keyword_cpp/cFile.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "cFile.h"
+
+void add (int a, int b) {
+	printf("sum of two numbers %d + %d = %d \n",a,b,a+b); 
+}
---
+++ extern_keyword_cpp/extern_program.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+extern "C" {
+#include "cFile.h"
+}
+int main() {
+	add(5,6);
+	return 0;
+}
